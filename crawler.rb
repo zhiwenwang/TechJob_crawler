4c7b30e1f2b2da90d549290bd29cd4c15744fb10
Cyan <wzwkak@gmail.com>
2015-12-23 16:24:21 +0800

    version 2

--- crawler.rb
+++ crawler.rb
@@ -1,14 +1,21 @@
+# encoding: UTF-8
 require 'nokogiri' 
 require 'json'
 require 'open-uri'
+require 'pp'
+require 'mysql2'
+
 host ="https://www.ptt.cc"
 doc = String
 data, link, post_id, articles_of_page= []
-doc_article, title, content_text = String
-#  previous_page = html[1]['href']#上頁
+doc_article, title, ptt_link, ptt_id = String
+json_file = "./ptt.json"
 count = Integer
+contents = String
+#  ptt_tag=%w(看板,標題,作者)
 
 def parse_content(url)
+client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "", :database => "demo_development")
     begin
         host ="https://www.ptt.cc" 
         count = 0
@@ -18,51 +25,47 @@ def parse_content(url)
         data.each do |link|
             link = data[count]['href']	#取陣列中"href"欄位
             post_id = link.split("/")[3]
-            title = data[count].text	
-            doc_article= Nokogiri::HTML(open(host+link))
-            content_text = doc_article.xpath("//div[@class='bbs-screen bbs-content']").text
-            count = count +1
-            articles_of_page.push({:post_id=>post_id, :link_url=>link, :title=>title, :content=>content_text})
+            ptt_link = host+link
+            ptt_id = link.match /[M].\d{10}\.[A-Z].\w{3}/
+            puts ptt_id
+            doc_article= Nokogiri::HTML(open(ptt_link))rescue OpenURI::HTTPError 
+            next if doc_article.nil?
+            content_text = doc_article.to_s
+#              contents = content_text[/<div id="main-content" class="bbs-screen bbs-content">(.+?)※ 發信站/m, 1].to_s
+            type = content_text[/\看板\<\/span><span class=\"article-meta-value\">(.*?)<\/span>/m, 1]
+            author = content_text[/<span class=\"article-meta-value\">(.+?)<\/span>/m, 1]
+            # 作者
+            title = content_text[/<span class=\"article-meta-tag\">標題<\/span><span class=\"article-meta-value\">(.*?)<\/span>/m, 1]
+            #標題
+            contents = content_text[/\w{3}\s\w{3}\s\d{2}\s\d{2}:\d{2}:\d{2}\s\d{4}(.+?)※ 發信站/m, 1].to_s
+            #內文
+            count +=1
+            result = client.query("INSERT INTO posts (ptt_post_id, ptt_post_link, name, content) VALUES ('#{ptt_id}', '#{ptt_link}', '#{title}', '#{contents.gsub("'","\\")}')")
+            sleep 1
         end
-#          puts "GET____ARTICLES:#{articles_of_page.length}"
-        articles_of_page.each do |a|
-            puts "TITLE____:#{a[:title]}"
+        rescue Errno::ECONNRESET =>e
+            puts e
+                sleep 3
+                retry
         end
-    rescue 
-        puts "FQ   REREY"
-        sleep 3
-        parse_content(url)
-    end
 end
 
-first_page = host + "/bbs/Tech_Job/index.html"
+first_page = host + "/bbs/Beauty/index.html"
 index_doc = Nokogiri::HTML(open(first_page))
 html = index_doc.css("//div[@class='btn-group pull-right']").css('/a[@class]').to_a
 next_page = html[2]['href']#下頁
 if next_page == nil
-    url = host+ "/bbs/Tech_Job/index.html"
+    url = host+ "/bbs/Beauty/index.html"
+    puts url
     parse_content(url)
 end
 page_number = html[1]['href'].split("/")[3][5..8].to_i
 page_number.times do |link_number|
-#  link_number = html[1]['href'].split("/")[3][5..8].to_i
-    
-    
-    
-    
 page_number -=1
-url = host+ "/bbs/Tech_Job/index#{page_number}.html"
+url = host+ "/bbs/Beauty/index#{page_number}.html"
 puts url 
 first_page = url
 parse_content(url)
+sleep 1
 break if page_number == 0
 end
-
-
-
-#              if html[1]['href'] == nil
-#                  url = index_url
-#                  url=host#{next_page}
-#                  parse_content(url)
-#              end
-#  end
